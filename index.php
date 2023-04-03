c854d8322dbd8cb368b7da75ea71250f1a483226
Lukas <Lukas.v.hulst@gmail.com>
2023-04-03 14:01:29 +0200

    eerste opdracht

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+echo "<h1> Hello World!</h1>";
+echo "Hello World! <br>";
+$txt = "Hello World! <br>";
+$txt2 = "Learning PHP";
+echo $txt;
+define ("txt3", "Hello World! <br>");
+$txt2 = txt3;
+echo $txt2;
+$hello = "Hello";
+$world = " World!";
+$prints ="<html> <h1> $hello, $world</h1> </html>";
+echo $prints;
