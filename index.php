3630a5c7beb66ba1fa3eed541d8ec3a43f8d4b5b
Lukas <Lukas.v.hulst@gmail.com>
2023-04-04 15:21:01 +0200

    random number math

--- index.php
+++ index.php
@@ -10,4 +10,12 @@ echo $txt2;
 $hello = "Hello";
 $world = " World!";
 $prints ="<html> <h1> $hello, $world</h1> </html>";
-echo $prints;
+echo $prints; 
+
+$num1 = rand(10,100);
+$num2 = rand(10,100);
+
+echo "$num1 + $num2 = " . ($num1 + $num2) . "<br>";
+echo "$num1 - $num2 = " . ($num1 - $num2) . "<br>";
+echo "$num1 / $num2 = " . ($num1 / $num2) . "<br>";
+echo "$num1 * $num2 = " . ($num1 * $num2) . "<br>";
